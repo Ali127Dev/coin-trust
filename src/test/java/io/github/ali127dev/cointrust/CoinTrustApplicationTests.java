b58076c9cb1c1462fb1e9e302298bf7b06902924
Ali127Dev <Ali127Dev@gmail.com>
2026-09-07 12:03:56 +0330

    init

---
+++ src/test/java/io/github/ali127dev/cointrust/CoinTrustApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ali127dev.cointrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinTrustApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
