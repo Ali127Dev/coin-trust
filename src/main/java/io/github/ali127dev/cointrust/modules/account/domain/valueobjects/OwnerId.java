6f04f0338e06ed2607bd76595d9621260056a375
Ali127Dev <Ali127Dev@gmail.com>
2026-09-09 20:46:03 +0330

    chore: make OwnerId class final

--- src/main/java/io/github/ali127dev/cointrust/modules/account/domain/valueobjects/OwnerId.java
+++ src/main/java/io/github/ali127dev/cointrust/modules/account/domain/valueobjects/OwnerId.java
@@ -6,17 +6,13 @@ import io.github.ali127dev.cointrust.shared.domain.valueobjects.Identifier;
 
 import java.util.UUID;
 
-public class OwnerId extends Identifier {
+public final class OwnerId extends Identifier {
     private OwnerId(UUID value) {
         super(value);
     }
 
-    public static OwnerId fromString(String rawOwnerId) {
-        try {
-            return new OwnerId(UUID.fromString(rawOwnerId));
-        } catch (IllegalArgumentException e) {
-            throw new InvalidOwnerIdException(rawOwnerId);
-        }
+    public static OwnerId generate() {
+        return new OwnerId(UUID.randomUUID());
     }
 
     public static OwnerId fromUuid(UUID uuid) {
