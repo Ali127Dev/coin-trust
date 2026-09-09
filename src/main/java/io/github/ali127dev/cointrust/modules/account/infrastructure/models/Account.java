7cab1075ff595d5e8348496e887e44e21733bcf2
Ali127Dev <Ali127Dev@gmail.com>
2026-09-09 22:53:30 +0330

    feat: add account db model

---
+++ src/main/java/io/github/ali127dev/cointrust/modules/account/infrastructure/models/Account.java
@@ -0,0 +1,29 @@
+package io.github.ali127dev.cointrust.modules.account.infrastructure.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "accounts")
+@Getter
+@NoArgsConstructor
+public class Account {
+    @Id
+    private UUID id;
+
+    private UUID ownerId;
+
+    private String address;
+
+    private long balance;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+}
