b26f4870a3b1c4551d791b88d591eedcf842012c
Ali127Dev <Ali127Dev@gmail.com>
2026-09-09 22:56:06 +0330

    feat: implement change address usecase

---
+++ src/main/java/io/github/ali127dev/cointrust/modules/account/application/usecases/ChangeAddressUsecase.java
@@ -0,0 +1,30 @@
+package io.github.ali127dev.cointrust.modules.account.application.usecases;
+
+import io.github.ali127dev.cointrust.modules.account.domain.contracts.AccountRepository;
+import io.github.ali127dev.cointrust.modules.account.domain.entities.Account;
+import io.github.ali127dev.cointrust.modules.account.domain.exceptions.AccountNotFoundException;
+import io.github.ali127dev.cointrust.modules.account.domain.valueobjects.AccountId;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ChangeAddressUsecase {
+    private final AccountRepository accountRepository;
+
+    @Transactional
+    public void execute(ChangeAddressInput input) {
+        AccountId accountId = AccountId.fromString(input.accountId());
+
+        Account account = accountRepository.findById(accountId)
+                .orElseThrow(AccountNotFoundException::new);
+
+        account.changeAddress(input.address());
+
+        accountRepository.save(account);
+    }
+
+    public record ChangeAddressInput(String accountId, String address) {
+    }
+}
