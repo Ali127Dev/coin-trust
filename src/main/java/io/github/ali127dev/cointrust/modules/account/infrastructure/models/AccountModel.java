a9c1727476a359ab51cb3a8a92ccfb9c77d78cd9
Ali127Dev <Ali127Dev@gmail.com>
2026-09-10 13:25:06 +0330

    fix: fix mapping account model issue

--- src/main/java/io/github/ali127dev/cointrust/modules/account/infrastructure/models/AccountModel.java
+++ src/main/java/io/github/ali127dev/cointrust/modules/account/infrastructure/models/AccountModel.java
@@ -3,9 +3,9 @@ package io.github.ali127dev.cointrust.modules.account.infrastructure.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -13,8 +13,8 @@ import java.util.UUID;
 @Entity
 @Table(name = "accounts")
 @Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class AccountModel {
     @Id
     private UUID id;
