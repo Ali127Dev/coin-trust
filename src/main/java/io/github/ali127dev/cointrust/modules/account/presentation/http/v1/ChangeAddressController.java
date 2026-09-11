6c42edc3e245c3f3cbb97349a1eda7180ff2ed08
Ali127Dev <Ali127Dev@gmail.com>
2026-09-11 14:37:36 +0330

    feat: implement change address controller

---
+++ src/main/java/io/github/ali127dev/cointrust/modules/account/presentation/http/v1/ChangeAddressController.java
@@ -0,0 +1,34 @@
+package io.github.ali127dev.cointrust.modules.account.presentation.http.v1;
+
+import io.github.ali127dev.cointrust.modules.account.application.usecases.ChangeAddressUsecase;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/v1/accounts")
+@RequiredArgsConstructor
+public class ChangeAddressController {
+    private final ChangeAddressUsecase changeAddressUsecase;
+
+    @PatchMapping("/addresses")
+    public ResponseEntity<Void> changeAddress(@RequestBody @Valid ChangeAddressInput dto) {
+        // TODO: Replace request-body ownerId with the authenticated user's ID from Spring SecurityContext.
+        var input = new ChangeAddressUsecase.ChangeAddressInput(
+                dto.ownerId(),
+                dto.address()
+        );
+
+        changeAddressUsecase.execute(input);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    public record ChangeAddressInput(@NotBlank String ownerId, @NotNull String address) {
+    }
+}
